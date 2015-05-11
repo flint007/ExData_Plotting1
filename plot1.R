dc1a7f0dfe1944f7331f71f2e122e5394f779e0d
Ralf <ralf@haiseba.com>
2015-05-10 20:16:28 -0700

    add data sample

--- plot1.R
+++ plot1.R
@@ -4,9 +4,13 @@ dest <- "household_power_consumption.zip"
 if (!file.exists(dest)) {
     #temp <- tempfile()
     download.file(input, dest, "curl")
+    #unlink(temp)
 }
 if (!file.exists("household_power_consumption.txt")) {
     untar(dest)
 }
-data <- read.table("household_power_consumption.txt")
-#unlink(temp)
+#data <- read.csv("household_power_consumption.txt")
+#Date;Time;Global_active_power;Global_reactive_power;Voltage;Global_intensity;Sub_metering_1;Sub_metering_2;Sub_metering_3
+#16/12/2006;17:24:00;4.216;0.418;234.840;18.400;0.000;1.000;17.000
+
+
