332dfe0dd31677b06b05bc7d1390a4affae0c6b9
Ralf <ralf@haiseba.com>
2015-05-11 21:36:34 -0700

    update plot1

--- plot1.R
+++ plot1.R
@@ -10,9 +10,12 @@ if (!file.exists("household_power_consumption.txt")) {
     untar(dest)
 }
 data <- read.csv("household_power_consumption.txt", sep=";", stringsAsFactors = FALSE)
-#Date;Time;Global_active_power;Global_reactive_power;Voltage;Global_intensity;Sub_metering_1;Sub_metering_2;Sub_metering_3
-#16/12/2006;17:24:00;4.216;0.418;234.840;18.400;0.000;1.000;17.000
+
+# Date;Time;Global_active_power;Global_reactive_power;Voltage;Global_intensity;Sub_metering_1;Sub_metering_2;Sub_metering_3
+# 16/12/2006;17:24:00;4.216;0.418;234.840;18.400;0.000;1.000;17.000
 x <- paste(data[,1], data[,2])
 data$date2 <- as.Date(strptime(x, "%d/%m/%Y %H:%M:%S"))
-dat <- subset(data, date2 > as.Date("2007-02-01") & date2 < as.Date("2007-02-03"))
-hist(as.numeric(dat$Global_active_power))
+subset <- subset(data, date2 == as.Date("2007-02-01") | date2 == as.Date("2007-02-02"))
+hist(as.numeric(subset$Global_active_power), main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+dev.copy(png,filename="plot1.png");
+dev.off()
